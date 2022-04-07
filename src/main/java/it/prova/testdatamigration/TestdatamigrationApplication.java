15c112617e2b1af2d7182d1e75e804023976161f
Kalos99 <calogerocorsello1999@gmail.com>
2022-04-07 14:06:17 +0200

    Primo commit creazione progetto

---
+++ src/main/java/it/prova/testdatamigration/TestdatamigrationApplication.java
@@ -0,0 +1,13 @@
+package it.prova.testdatamigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdatamigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdatamigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/testdatamigration/TestdatamigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.testdatamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdatamigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
